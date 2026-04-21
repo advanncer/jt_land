fbaa1e1d167e7b9829e3f15b38a140d8da5bbaf9
advanncer <advanncer@github.com>
2026-04-21 16:59:47 +0400

    fix: update build path for lp-check-up

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,19 @@ export default defineConfig({
         engAdultLp1: resolve(__dirname, "eng-adult/lp1/index.html"),
         engAdultLp1Tg: resolve(__dirname, "eng-adult/lp1-tg/index.html"),
         engAdultLpPains: resolve(__dirname, "eng-adult/lp_pains/index.html"),
-        comboLeadMagnit: resolve(__dirname, "free-products/combo-lead-magnit/index.html"),
-        astroEnglish: resolve(__dirname, "free-products/astro-english/index.html"),
+        comboLeadMagnit: resolve(
+          __dirname,
+          "free-products/combo-lead-magnit/index.html",
+        ),
+        astroEnglish: resolve(
+          __dirname,
+          "free-products/astro-english/index.html",
+        ),
         bloggerLp1: resolve(__dirname, "blogger/lp1/index.html"),
-        engAdultLpGoogle: resolve(__dirname, "eng-adult/lp_google/index.html"),
+        engAdultLpCheckUp: resolve(
+          __dirname,
+          "eng-adult/lp-check-up/index.html",
+        ),
       },
     },
   },
